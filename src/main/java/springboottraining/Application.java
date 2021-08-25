7f026dab62e9ee0511d14f28d14adf5ce89c7161
ACTON\qtdinh <qt.dinh@actongroup.com>
2021-08-25 18:01:16 +0200

    Init app

---
+++ src/main/java/springboottraining/Application.java
@@ -0,0 +1,22 @@
+package springboottraining;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
+public class Application {
+
+	private static final Logger logger = LoggerFactory.getLogger(Application.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+		logger.info("Application started :)");
+		System.out.println("Hello");
+	}
+
+}
